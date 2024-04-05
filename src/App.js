c80bf316cf6c50514077483969cbddd0bf8326cc
ChloePark85 <hapark85@gmail.com>
2024-04-05 18:46:12 +0900

    todo 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState(""); //인풋창에 들어가는 것
+
+  const onChangeNewTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const nextTodo = [
+      ...todo,
+      { title: newTodo, completed: false, id: Math.random() },
+    ];
+    setTodo(nextTodo); //불변성 immer 라이브러리
+    console.log(todo); //이전값, 비동기때문은 아님
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todo.length === 0 ? (
+        <div>
+          <div>Todo</div>
+          <form onSubmit={onSubmit}>
+            <input value={newTodo} onChange={onChangeNewTodo} />
+            <button>추가</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          {todo.map((t) => (
+            <div key={t.id}>{t.title}</div>
+          ))}
+          <form onSubmit={onSubmit}>
+            <input value={newTodo} onChange={onChangeNewTodo} />
+            <button>추가</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
